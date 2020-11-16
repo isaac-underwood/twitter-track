f50f9939a4600bf398b7f8b41a2e118d8dceb0f2
Isaac Underwood <isaacund220220@gmail.com>
2020-11-17 10:57:09 +1300

    Update pipelines to handle empty username input and give better output

--- frequencies.py
+++ frequencies.py
@@ -9,44 +9,47 @@ class Frequency:
         self.mongo = conn
 
     # Function returns a JSON of total tweets per year for given dates
-    def get_year_total(self, username, search_keyword, date_from, date_until):
+    def get_year_total(self, search_keyword, date_from, date_until, username=None):
         date_from = datetime.strptime(date_from, "%Y-%m-%d")
         date_until = datetime.strptime(date_until, "%Y-%m-%d")
-        self.set_year_pipeline(username, search_keyword, date_from, date_until)
-        results = self.mongo.test.aggregate(aggregate="test", pipeline=self.year_pipeline)
+        self.set_year_pipeline(search_keyword, date_from, date_until, username)
+        results = self.mongo.test.aggregate(aggregate="test", pipeline=self.year_pipeline, allowDiskUse=True)
         return results
 
     # Function returns a JSON of total tweets per month for given dates
-    def get_month_total(self, username, search_keyword, date_from, date_until):
+    def get_month_total(self, search_keyword, date_from, date_until, username=None):
         date_from = datetime.strptime(date_from, "%Y-%m-%d")
         date_until = datetime.strptime(date_until, "%Y-%m-%d")
-        self.set_month_pipeline(username, search_keyword, date_from, date_until)
-        results = self.mongo.test.aggregate(aggregate="test", pipeline=self.month_pipeline)
+        self.set_month_pipeline(search_keyword, date_from, date_until, username)
+        results = self.mongo.test.aggregate(aggregate="test", pipeline=self.month_pipeline, allowDiskUse=True)
         return results
 
     # Function returns a JSON of total tweets per week for given dates
-    def get_week_total(self, username, search_keyword, date_from, date_until):
+    def get_week_total(self, search_keyword, date_from, date_until, username=None):
         date_from = datetime.strptime(date_from, "%Y-%m-%d")
         date_until = datetime.strptime(date_until, "%Y-%m-%d")
-        self.set_week_pipeline(username, search_keyword, date_from, date_until)
+        self.set_week_pipeline(search_keyword, date_from, date_until, username)
         results = self.mongo.test.aggregate(aggregate="test", pipeline=self.week_pipeline)
         return results
 
     # Function returns a JSON of total tweets per day for given dates
-    def get_day_total(self, username, search_keyword, date_from, date_until):
+    def get_day_total(self, search_keyword, date_from, date_until, username=None):
         date_from = datetime.strptime(date_from, "%Y-%m-%d")
         date_until = datetime.strptime(date_until, "%Y-%m-%d")
-        self.set_day_pipeline(username, search_keyword, date_from, date_until)
+        self.set_day_pipeline(search_keyword, date_from, date_until, username)
         results = self.mongo.test.aggregate(aggregate="test", pipeline=self.day_pipeline)
         return results
 
     # Initialises the aggregation pipeline for year granularity
-    def set_year_pipeline(self, username, keyword, date_from, date_until):
-        #print(datetime.strptime(date_from, '%Y-%m-%d'))
+    def set_year_pipeline(self, keyword, date_from, date_until, username=None):
+        # Check if a username has been passed in, if not do not filter on username
+        match = {}
+        if username is None:
+            match = {"$match": {"$text": {"$search": keyword}}}
+        else:
+            match = {"$match": {"username": username, "$text": {"$search": keyword}}}
         self.year_pipeline = [
-            {
-                "$match": {"username": username, "$text": {"$search": keyword}}
-            },
+            match,  # The match filter (either containing username filter or not)
             {
                 "$project":
                 {
@@ -54,6 +57,8 @@ class Frequency:
                     "dateFrom": date_from,
                     "dateUntil": date_until,
                     "year": {"$year": {"$dateFromString": {"dateString": "$date"}}},
+                    "positiveSentiment": {"$strcasecmp": ['$sentiment_label', 'positive']},
+                    "negativeSentiment": {"$strcasecmp": ['$sentiment_label', 'negative']},
                 }
             },
             {
@@ -63,25 +68,57 @@ class Frequency:
                 "$group":
                 {
                     "_id": {"year": "$year"},
-                    "count": {"$sum": 1}
+                    "count": {"$sum": 1},
+                    "positive_sentiment": {
+                        "$sum": 
+                            {
+                                "$cond": 
+                                    [{
+                                        "$eq": [
+                                            "$positiveSentiment", 0
+                                        ]
+                                    }, 1, 0]}
+                    },
+                    "negative_sentiment": {
+                        "$sum": 
+                            {
+                                "$cond": 
+                                    [{
+                                        "$eq": [
+                                            "$negativeSentiment", 0
+                                        ]
+                                    }, 1, 0]}
+                    },
+                }
+            },
+            {
+                "$sort":
+                {
+                    "_id": 1
                 }
             }
         ]
 
     # Initialises the aggregation pipeline for month granularity
-    def set_month_pipeline(self, username, keyword, date_from, date_until):
+    def set_month_pipeline(self, keyword, date_from, date_until, username=None):
+        # Check if a username has been passed in, if not do not filter on username
+        match = {}
+        if username is None:
+            match = {"$match": {"$text": {"$search": keyword}}}
+        else:
+            match = {"$match": {"username": username, "$text": {"$search": keyword}}}
         self.month_pipeline = [
-            {
-                "$match": {"username": username, "$text": {"$search": keyword}}
-            },
+            match,  # The match filter (either containing username filter or not)
             {
                 "$project":
                 {
                     "wholeDate": {"$dateFromString": {"dateString": "$date"}},
                     "dateFrom": date_from,
                     "dateUntil": date_until,
-                    "year": {"$year": {"$dateFromString": {"dateString": "$date"}}},
                     "month": {"$month": {"$dateFromString": {"dateString": "$date"}}},
+                    "positiveSentiment": {"$strcasecmp": ['$sentiment_label', 'positive']},
+                    "negativeSentiment": {"$strcasecmp": ['$sentiment_label', 'negative']},
+
                 }
             },
             # {
@@ -90,66 +127,150 @@ class Frequency:
             {
                 "$group":
                 {
-                    "_id": {"year": "$year", "month": "$month"},
-                    "count": {"$sum": 1}
+                    "_id": {"month": "$month"},
+                    "count": {"$sum": 1},
+                    "positive_sentiment": {
+                        "$sum": 
+                            {
+                                "$cond": 
+                                    [{
+                                        "$eq": [
+                                            "$positiveSentiment", 0
+                                        ]
+                                    }, 1, 0]}
+                    },
+                    "negative_sentiment": {
+                        "$sum": 
+                            {
+                                "$cond": 
+                                    [{
+                                        "$eq": [
+                                            "$negativeSentiment", 0
+                                        ]
+                                    }, 1, 0]}
+                    },
+                }
+            },
+            {
+                "$sort":
+                {
+                    "_id": 1
                 }
             }
+
         ]
 
     # Initialises the aggregation pipeline for week granularity
-    def set_week_pipeline(self, username, keyword, date_from, date_until):
+    def set_week_pipeline(self, keyword, date_from, date_until, username=None):
+        # Check if a username has been passed in, if not do not filter on username
+        match = {}
+        if username is None:
+            match = {"$match": {"$text": {"$search": keyword}}}
+        else:
+            match = {"$match": {"username": username, "$text": {"$search": keyword}}}
         self.week_pipeline = [
-            {
-                "$match": {"username": username, "$text": {"$search": keyword}}
-            },
+            match,  # The match filter (either containing username filter or not)
             {
                 "$project":
                 {
                     "wholeDate": {"$dateFromString": {"dateString": "$date"}},
                     "dateFrom": date_from,
                     "dateUntil": date_until,
-                    "year": {"$year": {"$dateFromString": {"dateString": "$date"}}},
-                    "month": {"$month": {"$dateFromString": {"dateString": "$date"}}},
                     "week": {"$toInt": {"$round": {"$divide": [{"$dayOfMonth": {"$dateFromString": {"dateString": "$date"}}}, 7]}}},
+                    "positiveSentiment": {"$strcasecmp": ['$sentiment_label', 'positive']},
+                    "negativeSentiment": {"$strcasecmp": ['$sentiment_label', 'negative']},
+
                 }
             },
-            # {
-            #     "$match": {"wholeDate": {"$gte": "dateFrom", "$lte": "dateUntil"}}
-            # },
             {
                 "$group":
                 {
-                    "_id": {"year": "$year", "month": "$month", "week": "$week"},
-                    "count": {"$sum": 1}
+                    "_id": {"week": "$week"},
+                    "count": {"$sum": 1},
+                    "positive_sentiment": {
+                        "$sum": 
+                            {
+                                "$cond": 
+                                    [{
+                                        "$eq": [
+                                            "$positiveSentiment", 0
+                                        ]
+                                    }, 1, 0]}
+                    },
+                    "negative_sentiment": {
+                        "$sum": 
+                            {
+                                "$cond": 
+                                    [{
+                                        "$eq": [
+                                            "$negativeSentiment", 0
+                                        ]
+                                    }, 1, 0]}
+                    },
+                }
+            },
+            {
+                "$sort":
+                {
+                    "_id": 1
                 }
             }
+
         ]
 
     # Initialises the aggregation pipeline for day granularity
-    def set_day_pipeline(self, username, keyword, date_from, date_until):
+    def set_day_pipeline(self, keyword, date_from, date_until, username=None):
+        # Check if a username has been passed in, if not do not filter on username
+        match = {}
+        if username is None:
+            match = {"$match": {"$text": {"$search": keyword}}}
+        else:
+            match = {"$match": {"username": username, "$text": {"$search": keyword}}}
         self.day_pipeline = [
-            {
-                "$match": {"username": username, "$text": {"$search": keyword}}
-            },
+            match,  # The match filter (either containing username filter or not)
             {
                 "$project":
                 {
                     "wholeDate": {"$dateFromString": {"dateString": "$date"}},
                     "dateFrom": date_from,
                     "dateUntil": date_until,
-                    "year": {"$year": {"$dateFromString": {"dateString": "$date"}}},
-                    "month": {"$month": {"$dateFromString": {"dateString": "$date"}}},
                     "day": {"$dayOfMonth": {"$dateFromString": {"dateString": "$date"}}},
+                    "positiveSentiment": {"$strcasecmp": ['$sentiment_label', 'positive']},
+                    "negativeSentiment": {"$strcasecmp": ['$sentiment_label', 'negative']},
+
                 }
             },
-            # {
-            #     "$match": {"wholeDate": {"$gte": "dateFrom", "$lte": "dateUntil"}}
-            # },
             {
                 "$group":
                 {
-                    "_id": {"year": "$year", "month": "$month", "day": "$day"},
-                    "count": {"$sum": 1}
+                    "_id": {"day": "$day"},
+                    "count": {"$sum": 1},
+                    "positive_sentiment": {
+                        "$sum": 
+                            {
+                                "$cond": 
+                                    [{
+                                        "$eq": [
+                                            "$positiveSentiment", 0
+                                        ]
+                                    }, 1, 0]}
+                    },
+                    "negative_sentiment": {
+                        "$sum": 
+                            {
+                                "$cond": 
+                                    [{
+                                        "$eq": [
+                                            "$negativeSentiment", 0
+                                        ]
+                                    }, 1, 0]}
+                    },
+                }
+            },
+            {
+                "$sort":
+                {
+                    "_id": 1
                 }
             }
         ]
