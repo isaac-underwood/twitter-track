e7cde12d7706887f3e573912273dba03cfbd5be1
Isaac Underwood <isaacund220220@gmail.com>
2021-04-18 17:41:08 +1200

    Move NLP files to NLP repo

--- nlp/run_sa.py
+++
@@ -1,9 +0,0 @@
-from flask_pymongo import PyMongo
-from pymongo import MongoClient
-from sentiment_analysis import SentimentAnalysis
-
-client = MongoClient('localhost', 27017)
-db = client.test
-tweets = db.test
-sa = SentimentAnalysis(tweets)
-sa.process_tweets()
